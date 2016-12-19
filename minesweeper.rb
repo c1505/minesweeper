bd753fff370164b4e631476e9d5f34d9c8b06abd
c1505 <coreym247@gmail.com>
2016-12-19 22:41:05 +0000

    counting correctly

--- minesweeper.rb
+++ minesweeper.rb
@@ -9,14 +9,16 @@ class Board
   #   board.each do |row|
   #     row.chars.each do |space|
   #       Space.new
+  # i think the easy solution seems not easy now.  wasn't thinking about 8.  
   attr_accessor :board, :trimmed_board
   def initialize(board)
     @board = board
     @trimmed_board = trim_board
+    @out = board_of_zeros
   end
   
   def trim_board
-    first_step = @board.slice(1..@board.size - 2)
+    first_step = @board.slice(1..@board.size - 1)
     first_step.map do |row|
       row.slice(1..row.size - 2)
     end
@@ -31,29 +33,56 @@ class Board
   end
   
   def transform
-    out = board_of_zeros
-    binding.pry
     @trimmed_board.each_with_index do |row, v_index|
       row.chars.each_with_index do |space, h_index|
-        # if space == "*"
-        #   out[v_index][h_index + 1] = out[v_index][h_index + 1] + 1
-        #   out[v_index][h_index + -1] = out[v_index][h_index + -1] + 1
-
-        #   out[v_index + 1][h_index] = out[v_index + 1][h_index] + 1
-        #   out[v_index - 1 ][h_index + 1] = out[v_index + 1][h_index] + 1
-        # end
-        # out[v_index + 1][h_index] = space
-        # out[v_index - 1 ][h_index + 1] = space
+        if space == "*"
+          
+          score_row(v_index, h_index)
+          
+          score_other_row(v_index + 1, h_index)
+          # if @out[v_index + 1][h_index]
+          #   @out[v_index + 1][h_index] = @out[v_index + 1][h_index] + 1
+          # end
+          score_other_row(v_index - 1, h_index)
+          # if @out[v_index - 1 ][h_index + 1]
+          #   @out[v_index - 1 ][h_index + 1] = @out[v_index + 1][h_index] + 1
+          # end
+        end
       end
     end
-    out.pop.shift
-    out
+    @out.pop.shift
+    @out
   end
+  
+  def score_row(v_index, h_index)
+    row = @out[v_index]
+    if row[h_index + 1]
+      row[h_index + 1] = row[h_index + 1] + 1
+    end
+    if row[h_index + -1]
+      row[h_index + -1] = row[h_index + -1] + 1
+    end
+    @out[v_index] = row
+  end
+  
+  def score_other_row(v_index, h_index)
+    row = @out[v_index]
+    row[h_index] = row[h_index] + 1
+    if row[h_index + 1]
+      row[h_index + 1] = row[h_index + 1] + 1
+    end
+    if row[h_index + -1]
+      row[h_index + -1] = row[h_index + -1] + 1
+    end
+    @out[v_index] = row
+  end
+    
+  
     
   
   def self.transform(board)
     board = Board.new(board)
-    # board.transform
+    board.transform
     # out = board.board_of_zeros
     # board.each_with_index do |row, v_index|
     #   row.chars.each_with_index do |space, h_index|
