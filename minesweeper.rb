7c09e0b872b8b5b0f076b4a0b5ca4515b9655693
c1505 <coreym247@gmail.com>
2017-01-16 20:31:01 +0000

    1 passing test

--- minesweeper.rb
+++ minesweeper.rb
@@ -4,7 +4,66 @@ class Board
   def initialize(board)
     @board = board
     @trimmed_board = trim_board
-    @out = board_of_zeros
+    @output = board_of_zeros
+    @mine_locations = []
+  end
+  
+  def self.transform(board)
+    board = Board.new(board)
+    board.transform
+  end
+  
+  def transform
+    @trimmed_board.each_with_index do |row, v_index|
+      row.chars.each_with_index do |space, h_index|
+        if space == "*"
+          score_row(v_index, h_index)
+          score_row(v_index + 1, h_index)
+          score_row(v_index - 1, h_index)
+          @mine_locations << [v_index, h_index]
+        end
+      end
+    end
+    @output.pop #shouldn't need this.
+    replace_mines
+    add_sides
+    
+    string_and_join
+    remove_zeros
+    header_and_footer
+  end
+  
+  private
+  
+  def remove_zeros
+    @output.map! do |row|
+      row.gsub("0", " ")
+    end
+  end
+  
+  def string_and_join
+    @output.map! do |row|
+      row.map {|f| f.to_s }.join
+    end
+  end
+      
+  
+  def replace_mines
+    @mine_locations.each do |location|
+      @output[location[0]][location[1]] = "*"
+    end
+  end
+  
+  def add_sides
+    @output.each do |row|
+      row << '|'
+      row.unshift('|')
+    end
+  end
+  
+  def header_and_footer
+    @output << "+------+"
+    @output.unshift("+------+")
   end
   
   def trim_board
@@ -22,23 +81,8 @@ class Board
     end
   end
   
-  def transform
-    @trimmed_board.each_with_index do |row, v_index|
-      row.chars.each_with_index do |space, h_index|
-        if space == "*"
-          score_row(v_index, h_index)
-          score_row(v_index + 1, h_index)
-          score_row(v_index - 1, h_index)
-        end
-      end
-    end
-    @out.pop #shouldn't need this.
-    
-    @out
-  end
-  
   def score_row(v_index, h_index)
-    row = @out[v_index]
+    row = @output[v_index]
     row[h_index] = row[h_index] + 1
     if row[h_index + 1]
       row[h_index + 1] = row[h_index + 1] + 1
@@ -46,11 +90,7 @@ class Board
     if row[h_index + -1]
       row[h_index + -1] = row[h_index + -1] + 1
     end
-    @out[v_index] = row
+    @output[v_index] = row
   end
   
-  def self.transform(board)
-    board = Board.new(board)
-    board.transform
-  end
 end
